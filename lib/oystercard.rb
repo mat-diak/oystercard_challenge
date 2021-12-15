befb43d727ff5a06778025b1da3fb2888069b57c
chisambwe <c.m.kapihya@gmail.com>
2021-12-15 17:57:26 +0000

    WIP: saving entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,33 +1,50 @@
 class Oystercard
-    attr_reader :balance, :journey_status
+    attr_reader :balance, :journey_status, :entry_station
 
     LIMIT = 90
     MINIMUM_FARE = 1
     def initialize(balance = 0)
         @balance = balance
         @journey_status = false
+        @entry_station = nil
     end
 
     def top_up(amount)
-        (balance + amount) > LIMIT ? fail("Balance cannot exceed £#{LIMIT}.") : @balance += amount
+        (@balance + amount) > LIMIT ? fail("Balance cannot exceed £#{LIMIT}.") : @balance += amount
     end 
 
+    
+    def touch_in(station)
+        raise "Insufficient balance." unless @balance >= MINIMUM_FARE
+        @entry_station = station
+    end 
+    
+    def touch_out
+        deduct(MINIMUM_FARE)
+        @journey_status = false
+        @entry_station = nil
+    end
+    
+    def in_journey?
+        @journey_status
+        # if entry_station != then we are in journey!
+    end 
+    
+
+    private
+    
     def deduct(amount)
         @balance -= amount
     end
-        
-    def touch_in
-        # I want an error when balance is less than 1 else change journey status
-        raise "Insufficient balance." unless @balance >= MINIMUM_FARE
-        @journey_status = true 
-    end 
 
-    def touch_out
-        @journey_status = false
-
-    end
-
-    def in_journey?
-        @journey_status
-    end 
 end
+
+# GOAL - we wanna save the entry station
+# Where doo we get the station? "station"
+# Where do we store the station? @entry_station
+# at touch_in set @entry_station to "station"
+# at touch_out set @entry_station to nil
+# What is our double?
+
+# Goal - refactor in_journey 
+# in_journey relies on entry station
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,11 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
     
+    
     # check if Oystercard has all the methods
     it { is_expected.to respond_to(:top_up).with(1).argument}
-    it { is_expected.to respond_to(:deduct, :touch_out, :touch_in)}
-   
-    let(:card) { Oystercard.new }
+    it { is_expected.to respond_to(:touch_out, :touch_in, :entry_station, :balance)}
+    it { is_expected.not_to respond_to(:deduct) }
+    
+    let(:card) { Oystercard.new } # at this stage card does not exist it only saves a block to execute when card is called
+    let(:station) { double() } #creaing a station 
    
     context 'at start-up'do
         it 'sets balance to 0' do 
@@ -36,26 +39,38 @@ describe Oystercard do
         end
     end
 
-    describe '#deduct' do
+    # describe '#deduct' do
 
-        context "when their is suffecient balance" do
-            it 'deduct £50' do 
-                expect { card.deduct(50) }.to change(card, :balance).by(-50)
-            end 
-        end
-    end
+    #     context "when their is suffecient balance" do
+    #         it 'deduct £50' do 
+    #             expect { card.deduct(50) }.to change(card, :balance).by(-50)
+    #         end 
+    #     end
+    # end
 
     describe '#touch_in' do
         context 'when the balance is at least 1' do
             it 'changes in journey to true' do 
+                #arrange
                 card.top_up(1)
-                card.touch_in
+                #act
+                card.touch_in(station)
+                #assert
                 expect(card).to be_in_journey
             end 
+            it 'stores the entry station' do
+                #Arrage
+                card.top_up(20)
+                #act
+                card.touch_in(station)
+                #assert
+                expect(card.entry_station).to eq(station)
+            end
+
         end
         context 'when balance is 0' do
             it 'throws an error' do
-                expect { card.touch_in }.to raise_error "Insufficient balance."
+                expect { card.touch_in(station) }.to raise_error "Insufficient balance."
             end
         end
     end 
@@ -65,5 +80,35 @@ describe Oystercard do
             card.touch_out
             expect(card).not_to be_in_journey
         end 
+
+        it 'sets entry_station to nil' do
+            #Arrange
+            card.top_up(20)
+            #act
+            card.touch_in(station)
+            card.touch_out
+            #assert
+            expect(card.entry_station).to eq nil
+        end
+
+        it 'deducts minimum fare' do
+            #Arrange
+            # create a scenario
+            card.top_up(1)
+            card.touch_in(station)
+            #Assert (here act is card.touch_in)
+            expect { card.touch_out }.to change(card, :balance).by(-Oystercard::MINIMUM_FARE )
+        end
     end 
+
+    describe '#in_journey?' do
+        # when @entry_station isn't nil, we want @journey_status to be true
+        # arrange
+        # we want touch_in(station) to alter journey_status
+
+        #act
+
+        #assert
+    end
 end
+
